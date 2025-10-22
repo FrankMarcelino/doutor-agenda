34ad45c6566a3932e0af13ec6405dc5af2802760
Frank Silva <frank.silva.ti@gmail.com>
2025-10-22 14:59:23 -0300

    feat: implement DoctorsPage layout with structured components for better organization

--- src/app/(protected)/doctors/page.tsx
+++ src/app/(protected)/doctors/page.tsx
@@ -1,5 +1,36 @@
+import { Plus } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  PageActions,
+  PageContainer,
+  PageContent,
+  PageDescription,
+  PageHeader,
+  PageHeaderContent,
+  PageTitle,
+} from "@/components/ui/page-container";
+
 const DoctorsPage = () => {
-  return <div>Área dos médicos</div>;
+  return (
+    <PageContainer>
+      <PageHeader>
+        <PageHeaderContent>
+          <PageTitle>Médicos</PageTitle>
+          <PageDescription>Gerencie os médicos e sua clínicas</PageDescription>
+        </PageHeaderContent>
+        <PageActions>
+          <Button>
+            <Plus size={16} />
+            Adicionar Médico
+          </Button>
+        </PageActions>
+      </PageHeader>
+      <PageContent>
+        {/* Future content for managing doctors can be added here */}
+      </PageContent>
+    </PageContainer>
+  );
 };
 
 export default DoctorsPage;
