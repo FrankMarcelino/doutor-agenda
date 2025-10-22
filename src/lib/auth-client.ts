635e3ceaa1c861ea0566dad8bee3d4a2a678328c
Frank Silva <frank.silva.ti@gmail.com>
2025-10-22 12:29:07 -0300

    feat: integrate Radix UI avatar components and enhance authentication session with user clinic data

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,11 @@
-import { createAuthClient } from "better-auth/react"
+import { customSessionClient } from "better-auth/client/plugins";
+import { createAuthClient } from "better-auth/react";
+
+import { auth } from "./auth";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || "http://localhost:3000"
+  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || "http://localhost:3000",
+  plugins: [
+    customSessionClient<typeof auth>(),
+  ]
 });
